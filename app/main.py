06b9cf02443b9c30eac6755c451fd64b5b8a01f9
Joel Clarke <joel.clarke273@gmail.com>
2022-09-29 16:14:07 +0100

    4th stage attempt

--- app/main.py
+++ app/main.py
@@ -1,18 +1,24 @@
 import socket
+import threading
+
+def client_loop(connection):
+    while True:
+        connection.recv(1024) # wait for client to send data
+        connection.send(b"+PONG\r\n")
+
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_connection, _ = server_socket.accept() # wait for client
 
     while True:
-        client_connection.recv(1024) # wait for client to send data
-        client_connection.send(b"+PONG\r\n")
+        client_connection, _ = server_socket.accept() # wait for client
+        threading.Thread(target=client_loop, args=(client_connection,))
+
 
 
 if __name__ == "__main__":
