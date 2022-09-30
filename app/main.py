855793319f571a2e8424255f42433286e742558c
Joel Clarke <joel.clarke273@gmail.com>
2022-09-30 16:10:04 +0100

    Put RESP stuff in main file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
 import socket
 import threading
-from resp_handler import RESPDecoder
+
+class RESPDecoder:
+    def __call__(self, resp: bytes):
+        if resp.startswith(b"+"):
+            return self._simple_string(resp)
+        
+        elif resp.startswith(b"*"):
+            pass
+        
+        else:
+            raise Exception(f"Unknown data type byte: {resp[0]}")
+    
+    def _simple_string(resp: bytes):
+        return resp[1:].strip()
+
+
+class RESPEncoder:
+    pass
+
 
 def client_loop(connection):
     print("thread spawned")
