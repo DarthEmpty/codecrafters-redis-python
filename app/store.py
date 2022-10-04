47787b3cbf575ea861cf143050100eb4492813d8
Joel Clarke <joel.clarke273@gmail.com>
2022-10-04 16:52:46 +0100

    attempte #7

--- app/store.py
+++ app/store.py
@@ -1,9 +1,23 @@
+from time import time_ns
+
 class Store:
     def __init__(self):
         self.contents = {}
     
     def get(self, key):
-        return self.contents[key]
+        if key in self.contents:
+            value, expired = self.contents[key]
+            
+            if time_ns() > expired:
+                return value
+
+        return None
     
-    def set(self, key, value):
-        self.contents[key] = value
+    def set(self, key, value, expiry=None):
+        if expiry:
+            # expiry in millisecs, time_ns in nanosecs
+            expired = time_ns() + expiry * 1e6 
+            self.contents[key] = (value, expired)
+        
+        else:
+            self.contents[key] = (value, None)
